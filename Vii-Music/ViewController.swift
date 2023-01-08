ea543b5372ba5970e6542d555ea3b8d8d22f4649
Artem <artem-orlov92@bk.ru>
2023-01-08 21:22:40 +0300

    Initial Commit

---
+++ Vii-Music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vii-Music
+//
+//  Created by Артем Орлов on 08.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
