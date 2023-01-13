53ab554fd16a1448efb609da1d057653c2279d73
Alexander Altman <qewhouse@gmail.com>
2023-01-14 01:42:20 +0300

    segmented control

--- Vii-Music/Sources/Main/MainViewController.swift
+++ Vii-Music/Sources/Main/MainViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-
+    
     let headerLabel: UILabel = {
         let label = UILabel()
         label.text = "Hello, PUT-USER-Name"
@@ -30,7 +30,7 @@ final class MainViewController: UIViewController {
     }()
     
     let headerStackView: UIStackView = {
-       let stack = UIStackView()
+        let stack = UIStackView()
         stack.axis = .vertical
         stack.distribution = .equalSpacing
         stack.alignment = .leading
@@ -40,7 +40,7 @@ final class MainViewController: UIViewController {
     }()
     
     let searchBar: UISearchBar = {
-       let searchBar = UISearchBar()
+        let searchBar = UISearchBar()
         searchBar.searchTextField.layer.cornerRadius = 10
         searchBar.layer.cornerRadius = 10
         searchBar.layer.masksToBounds = true
@@ -49,54 +49,72 @@ final class MainViewController: UIViewController {
         return searchBar
     }()
     
-    let segmentedControl: UISegmentedControl = {
-       let segmentedControl = UISegmentedControl()
-        
-        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        return segmentedControl
+    let segmentedCOntrolItems = ["Recommendation", "Trending", "Rock", "Pop"]
+    let haptic = UISelectionFeedbackGenerator()
+    
+    lazy var segmentedControl: UISegmentedControl = {
+        let view = UISegmentedControl(items: segmentedCOntrolItems)
+        view.selectedSegmentIndex = 0
+        view.backgroundColor = .white
+        view.apportionsSegmentWidthsByContent = true
+        view.selectedSegmentTintColor = Theme.brightGreen
+        view.addTarget(self, action: #selector(indexChanged), for: .valueChanged)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
         setupViews()
         setConstraints()
-        
+        haptic.prepare()
     }
-   
+    
     
     func setupViews() {
+        navigationController?.navigationBar.isHidden = true
         view.backgroundColor = Theme.bgColor
-        
         view.addSubview(headerStackView)
         view.addSubview(searchBar)
-        view.addSubview(segmentedControl)
         headerStackView.addArrangedSubview(headerLabel)
         headerStackView.addArrangedSubview(headerSubLabel)
-        
+        view.addSubview(segmentedControl)
     }
     
+    
+    @objc func indexChanged() {
+        haptic.selectionChanged()
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            print("1")
+        case 1:
+            print("2")
+        case 2:
+            print("3")
+        default:
+            print("4")
+        }
+    }
     
     func setConstraints() {
         NSLayoutConstraint.activate([
             headerStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             headerStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            headerStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            headerStackView.bottomAnchor.constraint(equalTo: searchBar.topAnchor, constant: -10),
+            headerStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            headerStackView.bottomAnchor.constraint(equalTo: searchBar.topAnchor, constant: -20),
             
             searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            searchBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -500),
-            searchBar.heightAnchor.constraint(equalToConstant: 40)
+            //            searchBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -500),
+            searchBar.heightAnchor.constraint(equalToConstant: 40),
+            
+            segmentedControl.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 10),
+            segmentedControl.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            segmentedControl.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
             
             
         ])
-       
     }
-    
-    
-    
-    
 }
 
 import SwiftUI
