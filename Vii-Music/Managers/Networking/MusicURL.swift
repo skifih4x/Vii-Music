2adca7ce4da961251cee2ddaf8b722b812b1f383
Alexander Altman <qewhouse@gmail.com>
2023-01-17 22:45:38 +0300

    fix

---
+++ Vii-Music/Managers/Networking/MusicURL.swift
@@ -0,0 +1,10 @@
+//
+//  MusicURL.swift
+//  Vii-Music
+//
+//  Created by Alexander Altman on 17.01.2023.
+//
+
+import Foundation
+
+let musicURL = "https://itunes.apple.com/lookup?upc=720642462928&entity=song"
